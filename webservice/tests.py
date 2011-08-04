f01572a657cfb56d356efcb41db7257dad5398a1
Milton Madanda <milton@praekeltfoundation.org>
2011-08-04 14:36:38 +0200

    Trying to get the tests setup

--- webservice/tests.py
+++ webservice/tests.py
@@ -7,10 +7,25 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from ummeli.webservice.utils import APIClient
+from ummeli.webservice.models import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ApiTestCase(TestCase):
+    
+    def setUp(self):
+        self.client = APIClient()
+        self.client.login('milton','password')
+        #self.user = User.objects.get(username='milton')
+
+    def tearDown(self):
+        pass
+
+    def test_get_data(self):
+        resp = self.client.get(reverse('api:getuserdata', kwargs={'username':'milton'}))
+
+        from django.utils import simplejson
+        data = simplejson.loads(resp.content)
+        self.assertEquals(len(data), 1)
+        self.assertEquals(resp.status_code, 200)
