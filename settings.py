930930fbad0573fe0d594e23aef8258b74e1779c
Simon de Haan <simon@praekeltfoundation.org>
2011-08-10 12:47:02 +0200

    coverage

--- settings.py
+++ settings.py
@@ -121,8 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'django_nose',
     'south',
+    'django_nose',
 )
 
 # A sample logging configuration. The only tangible logging
