dfbe3a6dc8b41ede2566d1abda9996b63c0931b3
Milton Madanda <milton@praekeltfoundation.org>
2011-08-10 14:15:38 +0200

    updated test script to point to ummeli

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,6 @@
 virtualenv --no-site-packages ve && \
 source ve/bin/activate && \
     pip install -r requirements.pip && \
-    ./manage.py test --with-coverage --cover-erase --cover-package=core --cover-html --with-xunit && \
+    ./manage.py test --with-coverage --cover-erase --cover-package=ummeli --cover-html --with-xunit && \
     coverage xml --omit="ve/*" && \
 deactivate
