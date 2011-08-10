742b0f4bfdd18ffce769a9423c14d32218611f46
Simon de Haan <simon@praekeltfoundation.org>
2011-08-10 14:25:49 +0200

    removing old *.pyc files before running tests

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,7 @@
 virtualenv --no-site-packages ve && \
 source ve/bin/activate && \
     pip install -r requirements.pip && \
+    find ./ -name '*.pyc' -delete && \
     ./manage.py test --with-coverage --cover-erase --cover-package=ummeli --cover-html --with-xunit && \
     coverage xml --omit="ve/*" && \
 deactivate
