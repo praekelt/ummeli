0fcd1efc02003f32382ad4709f6c7b5776ff9eb5
Simon de Haan <simon@praekeltfoundation.org>
2011-08-10 13:56:48 +0200

    omitting ve from coverage info

--- run-tests.sh
+++ run-tests.sh
@@ -3,5 +3,5 @@ virtualenv --no-site-packages ve && \
 source ve/bin/activate && \
     pip install -r requirements.pip && \
     ./manage.py test --with-coverage --cover-erase --cover-package=core --cover-html --with-xunit && \
-    coverage xml && \
+    coverage xml --omit="ve/*" && \
 deactivate
