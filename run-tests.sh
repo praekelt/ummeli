72cdc098b1aa92a53d6f0b2ddd0d4f2e7966a32b
Simon de Haan <simon@praekeltfoundation.org>
2011-08-10 13:44:38 +0200

    handy-dandy test & coverage running script

---
+++ run-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+virtualenv --no-site-packages ve && \
+source ve/bin/activate && \
+    pip install -r requirements.pip && \
+    ./manage.py test --with-coverage --cover-erase --cover-package=core --cover-html --with-xunit && \
+    coverage xml && \
+deactivate
