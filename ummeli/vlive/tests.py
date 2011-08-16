cae95ef19212f83d24c24df6cecbb25b85750ec7
Milton Madanda <milton@praekeltfoundation.org>
2011-08-16 15:13:04 +0200

    added tests for login

--- ummeli/vlive/tests.py
+++ ummeli/vlive/tests.py
@@ -25,3 +25,21 @@ class VliveTestCase(TestCase):
         # to a login URL
         self.assertEquals(resp.get('Location', None), None)
         self.assertEquals(resp.status_code, 200)
+        
+    def test_login_view(self):
+        msisdn = '0123456789'
+        password = 'password'
+        user = User.objects.create_user(msisdn, '%s@domain.com' % msisdn, 
+                                        password)
+        resp = self.client.get(reverse('vlive:login'))
+        self.assertEquals(resp.status_code, 200)
+        
+        resp = self.client.post(reverse('vlive:login'), 
+                                {'username': msisdn, 'password': password})
+        self.assertEquals(resp.status_code, 302)
+        self.assertEquals(resp.get('Location', None), 'http://testserver/vlive')
+        
+        resp = self.client.post(reverse('vlive:login'), 
+                                {'username': 'unknown', 'password': password})
+        self.assertEquals(resp.status_code, 200)        
+        self.assertContains(resp, 'Sign in failed')
