543a3240914a336bd138e45c5ee9f04fd1aeb971
Milton Madanda <milton@praekeltfoundation.org>
2011-08-23 20:02:11 +0200

    added new files

---
+++ ummeli/vlive/utils.py
@@ -0,0 +1,16 @@
+import cStringIO as StringIO
+import ho.pisa as pisa
+from django.template.loader import get_template
+from django.template import Context
+from cgi import escape
+
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context)
+    result = StringIO.StringIO()
+
+    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
+    if pdf.err:
+        return None
+    return result.getvalue()
