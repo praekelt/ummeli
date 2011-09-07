faf2f6e8eaf096d366bf87d5e9c893704747cd78
Milton Madanda <milton@praekeltfoundation.org>
2011-09-07 14:06:42 +0200

    fixed login url after logging out

--- ummeli/vlive/urls.py
+++ ummeli/vlive/urls.py
@@ -2,12 +2,16 @@ from django.conf.urls.defaults import patterns, url
 from ummeli.api.handlers import UserHandler
 from ummeli.vlive import views, cv_views
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^login$', views.login, {'template_name': 'vlive/login.html'}, name='login'),
+    url(r'^login$', views.login, {'template_name': 'vlive/login.html'}, 
+        name='login'),
     url(r'^register$', views.register, name='register'),
-    url(r'^logout$', 'django.contrib.auth.views.logout_then_login', {'login_url': '../login'}, name='logout'),
+    url(r'^logout$', 'django.contrib.auth.views.logout_then_login', 
+        {'login_url': 'login'}, 
+        name = 'logout'),
     url(r'^edit$', views.edit, name='edit'),
     url(r'^edit/personal$', cv_views.personal_details),
     url(r'^edit/contact$', cv_views.contact_details),
