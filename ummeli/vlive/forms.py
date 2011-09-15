aa2e6cbfe63a2796fcd63cad4f14ee80093389ff
Milton Madanda <milton@praekeltfoundation.org>
2011-09-15 14:48:31 +0200

    updated form field names

--- ummeli/vlive/forms.py
+++ ummeli/vlive/forms.py
@@ -1,7 +1,7 @@
 from ummeli.api.models import (Certificate, Language, WorkExperience,
     Reference, CurriculumVitae)
 from django.forms import (ModelForm, CheckboxInput,  Form, EmailField,  
-                                            RegexField)
+                                            RegexField,  CharField)
 from django.forms.forms import BoundField
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
@@ -18,22 +18,35 @@ class PMLModelForm(ModelForm):
         return mark_safe(u'\n'.join(output))
 
 class PersonalDetailsForm(PMLModelForm):
+    firstName = CharField(label = 'Firstname')
+    dateOfBirth = CharField(label = 'Date of birth',  required=False)
     class Meta:
         model = CurriculumVitae
         fields = ('firstName', 'surname', 'dateOfBirth', 'gender')
 
 class ContactDetailsForm(PMLModelForm):
+    telephoneNumber= CharField(label = 'Phone number',  required=False)
+    email = CharField(label = 'Email address',  required=False)
+    houseNumber = CharField(label = 'House number',  required=False)
+    streetName = CharField(label = 'Street name',  required=False)
+    location = CharField(label = 'Location name',  required=False)
     class Meta:
         model = CurriculumVitae
         fields = ('telephoneNumber', 'email', 'houseNumber', 'streetName',
                 'location')
 
 class EducationDetailsForm(PMLModelForm):
+    highestGrade = CharField(label = 'Highest grade passed',  required=False)
+    highestGradeYear = CharField(label = 'Year passed',  required=False)
+    school = CharField(label = 'Name of school', required=False)
     class Meta:
         model = CurriculumVitae
         fields = ('highestGrade', 'highestGradeYear', 'school')
 
 class CertificateForm(PMLModelForm):
+    name = CharField(label = 'Name of Certificate')
+    institution = CharField(label = 'Name of institution', required=False)
+    year = CharField(label = 'Year completed', required=False)
     class Meta:
         model = Certificate
         
@@ -42,6 +55,8 @@ class WorkExperienceForm(PMLModelForm):
         model = WorkExperience
 
 class LanguageForm(PMLModelForm):
+    language = CharField(label = 'Name of language')
+    readWrite = CharField(label = 'Can you Read and Write in this language')
     class Meta:
         model = Language
         widgets = {'readWrite': CheckboxInput(),}
