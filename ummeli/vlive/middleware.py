68d4fa3bea8faa5bc1dcb8084e431870acd4d544
Milton Madanda <milton@praekeltfoundation.org>
2011-09-21 10:14:04 +0200

    added extra check in form post middleware

--- ummeli/vlive/middleware.py
+++ ummeli/vlive/middleware.py
@@ -4,6 +4,7 @@ class FormActionMiddleware(object):
     available to us via HTTP Headers
     """
     def process_request(self, request):
-        if (request.GET.get('_action',  None) == 'POST'):
+        msisdn = request.META.get('HTTP_X_UP_CALLING_LINE_ID', None)
+        if (request.GET.get('_action',  None) == 'POST' and msisdn != None):
             request.method = "POST"
             request.POST = request.GET
