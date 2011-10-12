5d3ff1c21f5186e88c20ddd2c5a6885af02b6004
Milton Madanda <milton@praekeltfoundation.org>
2011-10-12 11:00:28 +0200

    added integration with vumi

--- ummeli/vlive/tasks.py
+++ ummeli/vlive/tasks.py
@@ -1,7 +1,11 @@
 from celery.task import task
 from celery.task.sets import TaskSet
+from vumiclient.client import Client
 
 @task(ignore_result=True)
 def send_password_reset(msisdn, new_password):    
-    #TODO: send password using Vumi
-    return True
+    message = 'Ummeli on YAL :) Your new password is: %s' % new_password
+    client = Client('ummeli', '4mm3l1')
+    client.send_sms(to_msisdn = msisdn,  
+        from_msisdn = '', 
+        message = message)
