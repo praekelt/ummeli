d784d67be61787ed6c54c41dbdcc35b7f9268900
Milton Madanda <milton@praekeltfoundation.org>
2011-10-19 14:16:21 +0200

    removed print logging

--- ummeli/vlive/middleware.py
+++ ummeli/vlive/middleware.py
@@ -5,10 +5,7 @@ class FormActionMiddleware(object):
     """
     def process_request(self, request):
         msisdn = request.META.get('HTTP_X_UP_CALLING_LINE_ID', None)
-        print '--- begin log ---'
-        print request.COOKIES
-        print '--- end log ---'
-
+        
         if (request.GET.get('_action',  None) == 'POST' and msisdn != None):
             request.method = "POST"
             request.POST = request.GET
