067d315747639764b7999775314315816c5f6215
Simon de Haan <simon@praekeltfoundation.org>
2011-10-24 10:55:20 +0200

    specifying envs for fab

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,14 @@
 from fabric.api import *
 
-env.hosts = ['ubuntu@cloud.praekeltfoundation.org']
 env.path = '/var/praekelt/ummeli'
+
+def qa():
+    env.hosts = ['ubuntu@cloud.praekeltfoundation.org']
+
+def production():
+    env.hosts = ['ubuntu@ummeli.praekeltfoundation.org']
 
 def deploy():
     with cd(env.path):
         run('git pull')
         run('ve/bin/supervisorctl -c config/supervisord.conf restart all')
-    
