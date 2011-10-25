3baf981f2d2d2bcafd4db2c38ebbed5265bb5062
Simon de Haan <simon@praekeltfoundation.org>
2011-10-25 11:40:47 +0200

    contactNo -> contact_no, readWrite -> read_write

--- ummeli/vlive/forms.py
+++ ummeli/vlive/forms.py
@@ -1,7 +1,7 @@
 from ummeli.base.models import (Certificate, Language, WorkExperience,
     Reference, CurriculumVitae)
-from django.forms import (ModelForm, CheckboxInput,  Form, EmailField,  
-                                            RegexField,  CharField,  BooleanField,  IntegerField, 
+from django.forms import (ModelForm, CheckboxInput,  Form, EmailField,
+                                            RegexField,  CharField,  BooleanField,  IntegerField,
                                             Textarea)
 from ummeli.vlive.models import UserSubmittedJobArticle
 
@@ -14,71 +14,71 @@ def format_errors_as_pml(self):
     bf_errors = []
     for name,  field in self.fields.items():
         bf = BoundField(self, field, name)
-        
+
         if(bf.errors):
             for error in bf.errors:
-                bf_error = u'\n'.join([u'<TEXT position="ABOVE"><color value="red">%(label_name)s - %(error_message)s</color></TEXT><br/>' 
+                bf_error = u'\n'.join([u'<TEXT position="ABOVE"><color value="red">%(label_name)s - %(error_message)s</color></TEXT><br/>'
                             % {
-                            'label_name': conditional_escape(force_unicode(bf.label)), 
+                            'label_name': conditional_escape(force_unicode(bf.label)),
                             'error_message': conditional_escape(force_unicode(error))
                             }])
                 bf_errors.append(mark_safe(bf_error))
     return mark_safe(u'\n'.join(bf_errors))
-            
+
 def format_as_pml(self):
     output = []
     for name,  field in self.fields.items():
         bf = BoundField(self, field, name)
-        
+
         text_field_type = 'Text'
-        
+
         if(isinstance(field, IntegerField)):
             text_field_type = 'num'
-        
+
         field_str = ('<TEXT position="ABOVE">%(label_name)s</TEXT><FIELD name="%(field_name)s" type="%(text_field_type)s" default="%(field_value)s"/><br/>'
                 % {
                   'label_name': conditional_escape(force_unicode(bf.label)),
-                  'field_name': bf.html_name, 
-                  'field_value': bf.value()  if bf.value() != None  else '', 
+                  'field_name': bf.html_name,
+                  'field_value': bf.value()  if bf.value() != None  else '',
                   'text_field_type': text_field_type
                   })
         if(isinstance(field, BooleanField)):
             default = bf.value()  if bf.value() != None  else ''
-            
+
             field_str = ('''<CHOICE-GROUP type="radio" name="%(field_name)s">
             <TEXT>%(label_name)s</TEXT>
             <CHOICE value="True" %(default_true)s>Yes</CHOICE>
             <CHOICE value="False" %(default_false)s>No</CHOICE>
             </CHOICE-GROUP>''' %
             {
-                'default_true':  'checked="true"' if default else '', 
-                'default_false':  'checked="true"' if not default else '', 
+                'default_true':  'checked="true"' if default else '',
+                'default_false':  'checked="true"' if not default else '',
                 'label_name': conditional_escape(force_unicode(bf.label)),
-                'field_name': bf.html_name, 
-                'field_value': bf.value()  if bf.value() != None  else '' 
+                'field_name': bf.html_name,
+                'field_value': bf.value()  if bf.value() != None  else ''
             })
-        
+
         output.append(field_str)
     return mark_safe(u'\n'.join(output))
-        
+
 class PMLModelForm(ModelForm):
     def as_pml(self):
         return format_as_pml(self)
-        
+
     def errors_as_pml(self):
         return format_errors_as_pml(self)
 
 class PMLForm(Form):
     def as_pml(self):
         return format_as_pml(self)
-        
+
     def errors_as_pml(self):
         return format_errors_as_pml(self)
 
 class PersonalDetailsForm(PMLModelForm):
     firstName = CharField(label = 'Firstname')
     dateOfBirth = CharField(label = 'Date of birth',  required = False)
-    
+
     class Meta:
         model = CurriculumVitae
         fields = ('firstName', 'surname', 'dateOfBirth', 'gender')
@@ -89,7 +89,7 @@ class ContactDetailsForm(PMLModelForm):
     houseNumber = CharField(label = 'House number',  required = False)
     streetName = CharField(label = 'Street name',  required = False)
     location = CharField(label = 'Location name',  required = False)
-    
+
     class Meta:
         model = CurriculumVitae
         fields = ('telephoneNumber', 'email', 'houseNumber', 'streetName',
@@ -99,7 +99,7 @@ class EducationDetailsForm(PMLModelForm):
     highestGrade = CharField(label = 'Highest grade passed',  required = False)
     highestGradeYear = IntegerField(label = 'Year passed',  required = False)
     school = CharField(label = 'Name of school', required = False)
-    
+
     class Meta:
         model = CurriculumVitae
         fields = ('highestGrade', 'highestGradeYear', 'school')
@@ -108,33 +108,33 @@ class CertificateForm(PMLModelForm):
     name = CharField(label = 'Name of certificate')
     institution = CharField(label = 'Name of institution', required = False)
     year = IntegerField(label = 'Year completed', required = False)
-    
+
     class Meta:
         model = Certificate
-        
+
 class WorkExperienceForm(PMLModelForm):
     title = CharField(label = 'Job title')
     company = CharField(label = 'Name of company')
     startYear = IntegerField(label = 'Year started')
     endYear = IntegerField(label = 'Year ended')
-    
+
     class Meta:
         model = WorkExperience
 
 class LanguageForm(PMLModelForm):
     language = CharField(label = 'Name of language')
-    readWrite = BooleanField(label = 'Can you Read and Write in this language', 
+    read_write = BooleanField(label = 'Can you Read and Write in this language',
                                             required = False)
-    
+
     class Meta:
         model = Language
-        widgets = {'readWrite': CheckboxInput(),}
+        widgets = {'read_write': CheckboxInput(),}
 
 class ReferenceForm(PMLModelForm):
     fullname = CharField(label = 'Fullname')
     relationship = CharField(label = 'Relationship (title)')
-    contactNo = CharField(label = 'Contact number')
-    
+    contact_no = CharField(label = 'Contact number')
+
     class Meta:
         model = Reference
 
@@ -144,7 +144,7 @@ class EmailCVForm(PMLForm):
 
 class FaxCVForm(PMLForm):
     send_via = CharField(required = True)
-    send_to = RegexField('[0-9+]', required = True, 
+    send_to = RegexField('[0-9+]', required = True,
                                     error_message = 'Please enter a valid fax number.')
 
 class UserSubmittedJobArticleForm(PMLModelForm):
