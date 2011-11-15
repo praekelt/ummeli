a52914141b0449fa49a471a3b3878499d1f282f4
Milton Madanda <milton@praekeltfoundation.org>
2011-11-15 09:14:25 +0200

    added file extension to pdf attachment

--- ummeli/base/models.py
+++ ummeli/base/models.py
@@ -207,6 +207,6 @@ def schedule_cv_email(cv,  email_address,  email_text, from_address):
                                             from_address,
                                             [email_address], ['ummeli@praekeltfoundation.org'])
     pdf = render_to_pdf('pdf_template.html', {'model': cv})
-    email.attach('curriculum_vitae_for_%s_%s' % (cv.first_name, cv.surname),
+    email.attach('curriculum_vitae_for_%s_%s.pdf' % (cv.first_name, cv.surname),
                         pdf,  'application/pdf')
     email.send(fail_silently=False)
