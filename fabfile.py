461916de457ff46b9caceede44b9f31ec62c5812
Simon de Haan <simon@praekeltfoundation.org>
2011-11-24 15:47:11 +0200

    fix for fabfile due to supervisord rename

--- fabfile.py
+++ fabfile.py
@@ -3,13 +3,15 @@ from fabric.api import *
 env.path = '/var/praekelt/ummeli'
 
 def qa():
+    env.supervisord_file = 'supervisord.staging.conf'
     env.hosts = ['ubuntu@cloud.praekeltfoundation.org']
 
 def production():
+    env.supervisord_file = 'supervisord.production.conf'
     env.hosts = ['ubuntu@app1.praekeltfoundation.org']
 
 def deploy():
     with cd(env.path):
         run('git pull')
         run('kill -HUP `cat tmp/pids/gunicorn*.pid`')
-        run('supervisorctl -c config/supervisord.conf restart celery')
+        run('supervisorctl -c config/%(supervisord_file)s restart celery' % env)
