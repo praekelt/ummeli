fd22684854c5d87b57ef50f492d923b6cbc082ac
Milton Madanda <milton@praekeltfoundation.org>
2011-11-25 10:13:10 +0200

    fixed ummeli build on jenkins

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,9 @@ virtualenv --no-site-packages ve && \
 source ve/bin/activate && \
     pip install -r requirements.pip && \
     find ./ -name '*.pyc' -delete && \
-    ./ummeli/manage.py test --with-coverage --cover-erase --cover-package=ummeli --cover-html --with-xunit && \
+    cd ummeli/ && \
+    ./manage.py test --with-coverage --cover-erase --cover-package=ummeli --cover-html --with-xunit && \
+    cd .. && \
     coverage xml --omit="ve/*" && \
     (pyflakes ummeli/ > pyflakes.log || true) && \
     (pep8 ummeli/ > pep8.log || true ) && \
