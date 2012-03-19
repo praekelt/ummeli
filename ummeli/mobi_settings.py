381c02a3d5cfc531314e7f234b4556aa40532b2e
Milton Madanda <milton@praekeltfoundation.org>
2012-03-19 15:44:29 +0200

    another dev settings creep

--- ummeli/mobi_settings.py
+++ ummeli/mobi_settings.py
@@ -106,11 +106,11 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = abspath('media')
+STATIC_ROOT = abspath('ummeli-static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/media/./'
+STATIC_URL = '/ummeli-static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
