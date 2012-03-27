3517c021eaf533d12918f061cb94b0bb1db18e85
Milton Madanda <milton@praekeltfoundation.org>
2012-03-27 14:57:39 +0200

    updated ummeli to jmbo-dashboard

--- ummeli/settings.py
+++ ummeli/settings.py
@@ -171,7 +171,7 @@ INSTALLED_APPS = (
     #ummeli 2.0
     'django.contrib.flatpages',
     'django.contrib.comments',
-    'geckoboard',
+    'jmbodashboard',
     
     #jmbo
     'jmboarticles',
