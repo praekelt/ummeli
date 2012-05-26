28c3b21058bcd8f87f7a928a6e78003ca849bb43
Milton Madanda <milton@praekeltfoundation.org>
2012-05-26 12:26:15 +0200

    updated User Submitted Jobs admin

--- ummeli/base/admin.py
+++ ummeli/base/admin.py
@@ -5,6 +5,10 @@ from django.contrib import admin
 
 class UserSubmittedJobArticleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'text',)
+    list_display = ('date','title', 'text', 'province', 'job_category')
+    list_filter = ('province', 'job_category')
+    ordering = ('-date',)
+    date_hierarchy = 'date'
 
 admin.site.register(CurriculumVitae)
 admin.site.register(Certificate)
