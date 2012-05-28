323593deb9b813e6ce73a7390820d869d0340f50
Milton Madanda <milton@praekeltfoundation.org>
2012-05-28 16:59:56 +0200

    added comment alias to comment admin

--- ummeli/base/admin.py
+++ ummeli/base/admin.py
@@ -2,6 +2,9 @@ from ummeli.base.models import (Certificate, Language, WorkExperience,
     Reference, CurriculumVitae, CurriculumVitaeForm,  Article,  Province,  Category,
     UserSubmittedJobArticle)
 from django.contrib import admin
+from jmbocomments.models import UserComment
+from jmbocomments.admin import UserCommentAdmin
+
 
 class UserSubmittedJobArticleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'text',)
@@ -9,6 +12,18 @@ class UserSubmittedJobArticleAdmin(admin.ModelAdmin):
     list_filter = ('province', 'job_category')
     ordering = ('-date',)
     date_hierarchy = 'date'
+
+
+class UmmeliUserCommentAdmin(UserCommentAdmin):
+    list_display = ('content_object', 'user', 'comment_alias', 'comment',
+                    'submit_date', 'is_public', 'is_removed', 'like_count',
+                    'latest_flag')
+    list_filter = ('submit_date', 'site', 'is_public', 'is_removed')
+
+    def comment_alias(self, instance):
+        return instance.user.get_profile().fullname()\
+                if instance.user.get_profile().comment_as_anon\
+                else "Anon."
 
 admin.site.register(CurriculumVitae)
 admin.site.register(Certificate)
@@ -19,3 +34,7 @@ admin.site.register(Article)
 admin.site.register(Province)
 admin.site.register(Category)
 admin.site.register(UserSubmittedJobArticle, UserSubmittedJobArticleAdmin)
+
+if UserComment in admin.site._registry:
+    admin.site.unregister(UserComment)
+admin.site.register(UserComment, UmmeliUserCommentAdmin)
