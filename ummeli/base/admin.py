17c52bcc74a80acc019292e0c5344fa0a80126a1
Milton Madanda <milton@praekeltfoundation.org>
2012-06-18 14:09:46 +0200

    updated job article admin

--- ummeli/base/admin.py
+++ ummeli/base/admin.py
@@ -25,12 +25,19 @@ class UmmeliUserCommentAdmin(UserCommentAdmin):
                 if not instance.user.get_profile().comment_as_anon\
                 else "Anon."
 
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ('text',)
+    list_display = ('date', 'text', 'source')
+    list_filter = ('source',)
+    ordering = ('-date',)
+    date_hierarchy = 'date'
+
 admin.site.register(CurriculumVitae)
 admin.site.register(Certificate)
 admin.site.register(Language)
 admin.site.register(WorkExperience)
 admin.site.register(Reference)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Province)
 admin.site.register(Category)
 admin.site.register(UserSubmittedJobArticle, UserSubmittedJobArticleAdmin)
