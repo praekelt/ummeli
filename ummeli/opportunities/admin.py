508cd79be9968e1794b20464567a7745b06b0cfd
Milton Madanda <milton@praekeltfoundation.org>
2012-08-27 12:33:55 +0200

    added admin for models

--- ummeli/opportunities/admin.py
+++ ummeli/opportunities/admin.py
@@ -7,4 +7,9 @@ class OpportunityAdmin(ModelBaseAdmin):
     pass
 
 admin.site.register(Internship, OpportunityAdmin)
+admin.site.register(Volunteer, OpportunityAdmin)
+admin.site.register(Bursary, OpportunityAdmin)
+admin.site.register(Training, OpportunityAdmin)
+admin.site.register(Event, OpportunityAdmin)
+admin.site.register(Competition, OpportunityAdmin)
 admin.site.register(Salary)
