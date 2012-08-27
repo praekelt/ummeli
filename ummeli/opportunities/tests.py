57e5fb56b9c85789c25d5bfa13e2a07f58924df8
Milton Madanda <milton@praekeltfoundation.org>
2012-08-27 11:59:04 +0200

    added more tests for additional fields

--- ummeli/opportunities/tests.py
+++ ummeli/opportunities/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from ummeli.vlive.tests.utils import VLiveClient, VLiveTestCase
-from ummeli.opportunities.models import Internship, Salary
+from ummeli.opportunities.models import Internship, Salary, Training, Event
 
 
 class OpportunitiesTest(VLiveTestCase):
@@ -29,3 +29,29 @@ class OpportunitiesTest(VLiveTestCase):
         self.assertEqual(user.modelbase_set.filter(slug=i.slug).count(), 1)
         self.assertEqual(user.modelbase_set.all()[0].internship.salary.amount, 50)
         self.assertEqual(user.modelbase_set.all()[0].internship.education, 0)
+
+    def test_training(self):
+        self.login()
+        self.fill_in_basic_info()
+
+        user = User.objects.get(username=self.msisdn)
+
+        i = Training.objects.create(title='Test op',
+                                    description='This is a test',
+                                    owner=user,
+                                    cost=300)
+        self.assertEqual(user.modelbase_set.filter(slug=i.slug).count(), 1)
+        self.assertEqual(user.modelbase_set.all()[0].training.cost, 300)
+
+    def test_event(self):
+        self.login()
+        self.fill_in_basic_info()
+
+        user = User.objects.get(username=self.msisdn)
+
+        i = Event.objects.create(title='Test op',
+                                    description='This is a test',
+                                    owner=user,
+                                    location='Salt River')
+        self.assertEqual(user.modelbase_set.filter(slug=i.slug).count(), 1)
+        self.assertEqual(user.modelbase_set.all()[0].event.location, 'Salt River')
