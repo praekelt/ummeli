bbf95aed1636f0bc5bbe5ed7e18f8b7ffc50f64f
Milton <milton@praekeltfoundation.org>
2012-12-19 17:53:24 +0200

    allow specifying of user for deploy

--- fabfile.py
+++ fabfile.py
@@ -11,31 +11,31 @@ def production(user='ubuntu'):
     env.hosts = ['%s@cloud.praekeltfoundation.org' % user]
 
 def push():
-    run('sudo su %(user)s' % env)
+    run('sudo su ubuntu' % env)
     with cd(env.path):
         run('git pull')
 
 def static():
-    run('sudo su %(user)s' % env)
+    run('sudo su ubuntu' % env)
     with cd(env.path):
         run('ve/bin/python %(path)s/ummeli/manage.py collectstatic --noinput' % env)
 
 def migrate(app='ummeli.base'):
-    run('sudo su %(user)s' % env)
+    run('sudo su ubuntu' % env)
     with cd(env.path):
         env.app = app
         run('ve/bin/python %(path)s/ummeli/manage.py migrate %(app)s' % env)
 
 
 def deploy():
-    run('sudo su %(user)s' % env)
+    run('sudo su ubuntu' % env)
     with cd(env.path):
         run('git pull')
         run('kill -HUP `cat tmp/pids/u_gunicorn_*.pid`')
         run('sudo supervisorctl restart ummeli:u_celery' % env)
 
 def reload():
-    run('sudo su %(user)s' % env)
+    run('sudo su ubuntu' % env)
     with cd(env.path):
         run('git pull')
         run('kill -HUP `cat tmp/pids/u_gunicorn_*.pid`')
