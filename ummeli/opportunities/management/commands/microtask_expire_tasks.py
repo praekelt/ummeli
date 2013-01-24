e7c5d7be1ca68a9cfc82d1a86afb8840bcfeb6e8
Milton <milton@praekeltfoundation.org>
2013-01-24 09:00:11 +0200

    use celery beat for scheduling microtask expiry

--- ummeli/opportunities/management/commands/microtask_expire_tasks.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from ummeli.opportunities.models import MicroTask
-
-
-class Command(BaseCommand):
-    help = 'Expire micro tasks that have been checked out for longer than limit'
-
-    def handle(self, **options):
-        MicroTask.expire_tasks()
