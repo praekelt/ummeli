7f35f7bdd42ab4b721ec59f6b6aa260b51d0ec73
Milton <milton@praekeltfoundation.org>
2013-02-06 12:27:16 +0200

    updated my tasks to show different states

--- ummeli/vlive/inbox/urls.py
+++ ummeli/vlive/inbox/urls.py
@@ -4,5 +4,6 @@ from ummeli.vlive.inbox import views
 
 urlpatterns = patterns('',
     url(r'^$', views.inbox, name='my_inbox'),
-    url(r'^my_microtasks/$', views.my_microtasks, name='my_microtasks'),
+    url(r'^my_microtasks/$', views.MyTaskCheckoutListView.as_view(),
+        name='my_microtasks'),
     )
--- ummeli/vlive/inbox/views.py
+++ ummeli/vlive/inbox/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 from ummeli.opportunities.models import TaskCheckout
 
 
@@ -14,12 +15,10 @@ def inbox(request):
     return render(request, 'inbox/index.html', context)
 
 
-@login_required
-def my_microtasks(request):
-    context = {
-        'tasks_out': TaskCheckout.objects.filter(user=request.user,
-                                                state=0),
-        'tasks_expired': TaskCheckout.objects.filter(user=request.user,
-                                                state=2),
-        }
-    return render(request, 'inbox/my_microtasks.html', context)
+class MyTaskCheckoutListView(ListView):
+    paginate_by = 10
+    template_name = 'inbox/my_microtasks.html'
+
+    def get_queryset(self):
+        return TaskCheckout.objects.filter(user=self.request.user)\
+                                    .order_by('state')
