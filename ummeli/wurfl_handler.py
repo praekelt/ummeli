c4489afaad216e77e0e311c10bc74e7a45b5449e
Milton <milton@praekeltfoundation.org>
2013-02-25 10:50:41 +0200

    treat all android as high end devices

--- ummeli/wurfl_handler.py
+++ ummeli/wurfl_handler.py
@@ -4,6 +4,12 @@ from devproxy.handlers.wurfl_handler.base import WurflHandler
 class UmmeliWurflHandler(WurflHandler):
 
     def handle_device(self, request, device):
+        if device.brand_name == 'Android':
+            return [{
+                self.header_name: 'high',
+                'X-UA-brand-name': device.brand_name,
+                }]
+
         if device.resolution_width <= 240:
             return [{
                 self.header_name: 'medium',
