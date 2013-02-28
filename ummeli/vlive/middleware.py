15fd48a83263f9e1eb687c06d78a4742208d4da0
Milton <milton@praekeltfoundation.org>
2013-02-28 10:11:19 +0200

    dont show duplicate messages

--- ummeli/vlive/middleware.py
+++ ummeli/vlive/middleware.py
@@ -1,8 +1,12 @@
 from django.contrib import messages
 
+
 class AddMessageToResponseMiddleware(object):
     def process_response(self, request,  response):
-        if (request.method == 'POST' and 
+        if request.POST.get('ignore_post_success') == 'True':
+            return response
+
+        if (request.method == 'POST' and
             (response.status_code == 301 or response.status_code == 302)):
                 messages.add_message(request, messages.SUCCESS, 'Submitted successfully.')
         return response
