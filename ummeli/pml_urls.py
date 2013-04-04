afc99dbbb1b35412fea56fe1f75b34fbc178dea6
Milton <milton@praekeltfoundation.org>
2013-04-04 09:21:54 +0200

    fix admin landing on invalid url

--- ummeli/pml_urls.py
+++ ummeli/pml_urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from ummeli.vlive import views
+from django.views.generic.simple import redirect_to
 
 admin.autodiscover()
 
@@ -11,7 +12,9 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^admin/None/$', redirect_to, {'url': '/admin/'}),
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^health/$', views.health, name="health"),
     url(r'^stats/$', views.stats, name="stats"),
     url(r'^geckoboard/', include('jmbodashboard.geckoboard.urls')),
