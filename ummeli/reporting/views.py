bf9e6bc65ae463ca0e7b038fa285a0e6e7a4ef52
Milton <madandat@gmail.com>
2013-09-20 09:25:15 +0200

    added templatetags for reporting

--- ummeli/reporting/views.py
+++ ummeli/reporting/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseNotFound
 from reporting import helpers
 from jmbo.models import ModelBase
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 
 def report(request, slug, report_key_field):
@@ -14,4 +15,5 @@ def report(request, slug, report_key_field):
         report_key_field
     )
 
+    messages.success(request, 'Thank you. Your vote has been cast.')
     return redirect(request.META['HTTP_REFERER'])
