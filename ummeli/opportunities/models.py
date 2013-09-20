a7952cb1f055fe4bd0861319cc4e5de9eb4873f0
Milton <madandat@gmail.com>
2013-09-20 09:27:35 +0200

    use underscores for field values

--- ummeli/opportunities/models.py
+++ ummeli/opportunities/models.py
@@ -74,9 +74,10 @@ class Salary(models.Model):
 
 
 class UmmeliOpportunity(ModelBase):
-    ABUSE_REPORT_KEY_FIELD = 'ummeli-abuse'
-    INAPPROPRIATE_REPORT_KEY_FIELD = 'ummeli-inappropriate'
-    SCAM_REPORT_KEY_FIELD = 'ummeli-scam'
+    ABUSE_REPORT_KEY_FIELD = 'ummeli_abuse'
+    INAPPROPRIATE_REPORT_KEY_FIELD = 'ummeli_inappropriate'
+    SCAM_REPORT_KEY_FIELD = 'ummeli_scam'
+    POSITION_FILLED_REPORT_KEY_FIELD = 'ummeli_position_filled'
 
     province = models.ManyToManyField(
                     Province,
